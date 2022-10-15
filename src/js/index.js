c34046a721088415dd90dc7da18b8528c9f7cf8e
Fermain <Fermain@users.noreply.github.com>
2022-10-15 13:55:36 +0200

    Adds listeners

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+import { dadJoke } from './joker.js';
+
+const jokeContainer = document.querySelector('blockquote');
+const jokeButton = document.querySelector('button.dad-joke');;
+const jokeButtonText = jokeButton.querySelector('.joke-text');
+const originalText = jokeButtonText.textContent;
+
+jokeButton.addEventListener('click', async function () {
+  jokeButtonText.textContent = 'Loading...';
+  jokeButton.disabled = true;
+  jokeButton.classList.add('loading');
+
+  const joke = await dadJoke();
+
+  jokeButton.disabled = false;
+  jokeButton.classList.remove('loading');
+  jokeButtonText.textContent = originalText;
+  jokeContainer.textContent = joke;
+});
