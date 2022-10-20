950f0435f60bb6d8d861621aa1dc23b995f3a216
Oliver Dipple <Fermain@users.noreply.github.com>
2022-10-20 12:27:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function test() {
-  console.log("__BEARER_TOKEN__");
-  return "__BEARER_TOKEN__";
-}
+console.log("Getting a secret, then making a quote");
 
-test()
+// const out = document.querySelector("div#quote");
+    
+function test() {
+    console.log("__BEARER_TOKEN__");
+    return "__BEARER_TOKEN__";
+};
+
+const token = test();
+console.log(token);
+
+const API_BASE_URL = "https://nf-api.onrender.com/";
+const api = `${API_BASE_URL}/api/v1/quotes`;
+
+const options = {
+    headers: {
+        Authorization: `Bearer ${token}`,
+    },
+};
+
+console.log(api, options);
+getStuff(api, options);
+
+async function getStuff(api, options) {
+    const response = await fetch(api, options);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);    
+}
