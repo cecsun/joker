738105700adc06e507c3b791176d26ef44737cc0
cecsun <ceciliesunde94@gmail.com>
2024-01-10 12:56:26 +0100

    overwrite code

--- src/js/joker.js
+++ src/js/joker.js
@@ -1,13 +1,7 @@
-import joker from 'give-me-a-joke';
+import joker from "give-me-a-joke";
 
 export function dadJoke() {
-  return new Promise(function (resolve, reject) {
-    try {
-      joker.getRandomDadJoke(function (joke) {
-        resolve(joke);
-      });
-    } catch (error) {
-      reject(error);
-    }
-  });
+  return new Promise(function(resolve, reject) {
+    joker.getRandomDadJoke(resolve);
+  })
 }
